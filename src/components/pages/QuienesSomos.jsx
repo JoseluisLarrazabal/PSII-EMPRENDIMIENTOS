5ce12adcc5b5e0c388d52a55f9823750a7392a9d
JoseLuis <lfj1009417@est.univalle.edu>
2025-04-16 22:48:36 -0400

    Implementacion de el componente MissionStatement.jsx en QuienesSomos.jsx

--- src/components/pages/QuienesSomos.jsx
+++ src/components/pages/QuienesSomos.jsx
@@ -1,6 +1,7 @@
 // src/components/pages/QuienesSomos.jsx
 import React from "react";
 import HeroBanner from "../HeroBanner";
+import MissionStatement from "../MissionStatement"; // Asegúrate de que la ruta sea correcta   
 
 function QuienesSomos() {
   return (
@@ -11,6 +12,9 @@ function QuienesSomos() {
         title="Bienvenido a INCUVALAB" 
       />
       
+      {/* Declaración de misión */}
+      <MissionStatement />
+
       {/* Contenido principal */}
       <div className="max-w-[1200px] mx-auto px-6 md:px-12 py-8">
         <h1 className="text-3xl font-bold mb-8">Quiénes Somos</h1>
