19aca6c004cb41e0b9361953379a25fbf6ab053f
JoseLuis <lfj1009417@est.univalle.edu>
2025-04-27 20:18:06 -0400

    Borrada seccion de certificaciones a pedido del cliente

--- src/components/pages/MoocsPage.jsx
+++ src/components/pages/MoocsPage.jsx
@@ -4,8 +4,7 @@ import HeroSearch from '../../components/HeroSearch';
 import CourseCategory from '../../components/CourseCategory';
 import FilterSection from '../../components/FilterSection';
 import { executiveEducationCourses, mastersDegrees, bachelorDegrees, 
-         popularCourses, newCourses, trendingCourses, subjectFilters,
-         programFilters, schoolFilters } from './Moocs/data';
+         popularCourses, newCourses, trendingCourses, subjectFilters, schoolFilters } from './Moocs/data';
 
 const MoocsPage = () => {
   return (
@@ -26,14 +25,6 @@ const MoocsPage = () => {
       <CourseCategory title="Explore courses and programs" courses={popularCourses} />
       <CourseCategory title="New" courses={newCourses} />
       <CourseCategory title="Trending" courses={trendingCourses} />
-      
-      {/* Using FilterSection for programs */}
-      <FilterSection 
-        title="Filter by programs"
-        subtitle="Get a degree or certificate with us online"
-        type="programs"
-        items={programFilters} 
-      />
       
       {/* Using FilterSection for schools */}
       <FilterSection 
